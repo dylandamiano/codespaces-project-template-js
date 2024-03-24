7838daf7780c883e673666ba21e2607adcee1faf
Dylan Damiano <95459357+dylandamiano@users.noreply.github.com>
2024-03-24 03:08:16 +0000

    new

---
+++ src/Components/Education.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Education = () => {
+    return(
+        <section className="light" id="education">
+            <h2>Education</h2>
+            
+            <div className="container">
+                <div className="box">
+                    <h3>University of California, Santa Cruz</h3>
+                    <p className="small">B.S. in Computer Science</p>
+                    <p className="small">B.S. in Mathematics</p>
+                    <p className="small">Graduated: June 2022</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Education;
