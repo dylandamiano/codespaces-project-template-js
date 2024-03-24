9185728e5f4049a107da59152db1e9d5c39702c0
Dylan Damiano <95459357+dylandamiano@users.noreply.github.com>
2024-03-24 03:09:05 +0000

    fix

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -4,13 +4,13 @@ const Education = () => {
     return(
         <section className="light" id="education">
             <h2>Education</h2>
-            
+
             <div className="container">
                 <div className="box">
-                    <h3>University of California, Santa Cruz</h3>
-                    <p className="small">B.S. in Computer Science</p>
-                    <p className="small">B.S. in Mathematics</p>
-                    <p className="small">Graduated: June 2022</p>
+                    <h3>Central State University</h3>
+                    <p className="small">B.S. Program for Computer Science</p>
+                    <p className="small">B.S. Program for in Mathematics</p>
+                    <p className="small">Graduated: In Progress</p>
                 </div>
             </div>
         </section>
